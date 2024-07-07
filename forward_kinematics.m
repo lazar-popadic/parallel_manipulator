0448abf67be8b12a1e7d6e55a21ea00831432bfa
lazar-popadic <lazuskica@gmail.com>
2024-07-07 13:03:52 +0200

    rucne jednacine

--- forward_kinematics.m
+++ forward_kinematics.m
@@ -1,4 +1,4 @@
-function [xt,yt,x2,y2,x3,y3,x4,y4,x5,y5] = calculate_tcp(left_ang_deg, right_ang_deg)
+function [xt,yt,x2,y2,x3,y3,x4,y4,x5,y5] = forward_kinematics(left_ang_deg, right_ang_deg)
     % constant parameters
     L = 40;              % length of the static link [mm]
     P = 56;             % length of the proximal links [mm]
@@ -22,13 +22,22 @@ function [xt,yt,x2,y2,x3,y3,x4,y4,x5,y5] = calculate_tcp(left_ang_deg, right_ang
     y5 = y4 + P * sin(theta4rad);
 
     % Equations to solve
+    %syms theta3 theta5
+    % eq1 = x3 + D * cos(theta3) == x5 + D * cos(theta5);
+    % eq2 = y3 + D * sin(theta3) == y5 + D * sin(theta5);
 
-    syms theta3 theta5
-    eq1 = x3 + D * cos(theta3) == x5 + D * cos(theta5);
-    eq2 = y3 + D * sin(theta3) == y5 + D * sin(theta5);
+    % Support variables
+    A = (x5 - x3) / D;
+    B = (y5 - y3) / D;
+    C = (A*A + B*B) / 2;
 
     % Solve equations
-    [sol_theta3, sol_theta5] = solve([eq1, eq2], [theta3, theta5]);
+    % [sol_theta3, sol_theta5] = solve([eq1, eq2], [theta3, theta5]);
+    sol_theta5(1) = wrapToPi(2*atan2(-B+sqrt(A*A+B*B-C*C), C-A));
+    sol_theta5(2) = wrapToPi(2*atan2(-B-sqrt(A*A+B*B-C*C), C-A));
+
+    sol_theta3(1) = asin((y5-y3)/D + sin( sol_theta5(1)));
+    sol_theta3(2) = asin((y5-y3)/D + sin( sol_theta5(2)));
 
     % Take the right solution for +- configuration
 
@@ -51,10 +60,10 @@ function [xt,yt,x2,y2,x3,y3,x4,y4,x5,y5] = calculate_tcp(left_ang_deg, right_ang
 
     % Display the solutions
 
-    % fprintf('\nResults for checking with Fusion 360:\n\n');
-    % fprintf('Left input angle: \t\t\t\t%.2f degrees\n', -theta2);
-    % fprintf('Right input angle:  \t\t\t\t%.2f degrees\n', -theta4);
-    % fprintf('Left proximal angle in regards to left input: \t%.2f degrees\n', -theta3solved + theta2);
-    % fprintf('Right proximal angle in regards to right input: \t%.2f degrees\n\n', -theta5solved + theta4);
-    % fprintf('\nCoordinates of Tcp: \t\t(%.2f mm, %.2f mm)\n', xt, yt);
+    fprintf('\nResults for checking with Fusion 360:\n\n');
+    fprintf('Left input angle: \t\t\t\t%.2f degrees\n', -theta2);
+    fprintf('Right input angle:  \t\t\t\t%.2f degrees\n', -theta4);
+    fprintf('Left proximal angle in regards to left input: \t%.2f degrees\n', -theta3solved + theta2);
+    fprintf('Right proximal angle in regards to right input: \t%.2f degrees\n\n', -theta5solved + theta4);
+    fprintf('\nCoordinates of Tcp: \t\t(%.2f mm, %.2f mm)\n', xt, yt);
 end
