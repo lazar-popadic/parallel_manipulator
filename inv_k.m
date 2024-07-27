b8a9b76ce3228d7204103cf778acc90d6f54a287
lazar-popadic <lazuskica@gmail.com>
2024-07-27 11:05:10 +0200

    ik nova notacija

--- inv_k.m
+++ inv_k.m
@@ -1,37 +1,37 @@
 function [left_ang_deg, right_ang_deg] = inv_k(xt,yt)
-    [l,p,d] = get_L_P_D();
+    [L,P,D] = get_L_P_D();
     % Coordinates of the input joints
-    x2 = -l/2;          % x-coordinate of the left input joint
-    y2 = 0;              % y-coordinate of the left input joint
-    x5 = l/2;         % x-coordinate of the right input joint
+    x1 = -L/2;          % x-coordinate of the left input joint
+    y1 = 0;              % y-coordinate of the left input joint
+    x5 = L/2;         % x-coordinate of the right input joint
     y5 = 0;              % y-coordinate of the right input joint
 
     % Additional variables
-    A = xt - x2;
-    B = yt - y2;
-    C1 = (A*A + B*B + p*p - d*d) / (2*B*p);
+    A = xt - x1;
+    B = yt - y1;
+    C1 = (A*A + B*B + P*P - D*D) / (2*B*P);
     C2 = A / B;
 
     C = xt - x5;
-    D = yt - y5;
-    C3 = (C*C + D*D + p*p - d*d) / (2*D*p);
-    C4 = C / D;
+    D1 = yt - y5;
+    C3 = (C*C + D1*D1 + P*P - D*D) / (2*D1*P);
+    C4 = C / D1;
 
     % Solve ik
-    sol_theta2(1) = 2*atan2(1+sqrt(1-C1*C1+C2*C2), C1+C2);
-    sol_theta2(2) = 2*atan2(1-sqrt(1-C1*C1+C2*C2), C1+C2);
-    %sol_theta2(1) = wrapToPi(sol_theta2(1));
-    %sol_theta2(2) = wrapToPi(sol_theta2(2));
+    sol_thetaL(1) = 2*atan2(1+sqrt(1-C1*C1+C2*C2), C1+C2);
+    sol_thetaL(2) = 2*atan2(1-sqrt(1-C1*C1+C2*C2), C1+C2);
+    %sol_thetaL(1) = wrapToPi(sol_thetaL(1));
+    %sol_thetaL(2) = wrapToPi(sol_thetaL(2));
 
-    sol_theta5(1) = 2*atan2(1+sqrt(1-C3*C3+C4*C4), C3+C4);
-    sol_theta5(2) = 2*atan2(1-sqrt(1-C3*C3+C4*C4), C3+C4);
-    sol_theta5(1) = wrapToPi(sol_theta5(1));
-    sol_theta5(2) = wrapToPi(sol_theta5(2));
+    sol_thetaR(1) = 2*atan2(1+sqrt(1-C3*C3+C4*C4), C3+C4);
+    sol_thetaR(2) = 2*atan2(1-sqrt(1-C3*C3+C4*C4), C3+C4);
+    sol_thetaR(1) = wrapToPi(sol_thetaR(1));
+    sol_thetaR(2) = wrapToPi(sol_thetaR(2));
 
      % Take the right solution for +- configuration
-     % TODO: izracunaj i theta3 i 4 zbog ovoga
+     % TODO: izracunaj i theta3 i 4R zbog ovoga
      
 
-    left_ang_deg = rad2deg(double(sol_theta2(1)));
-    right_ang_deg = rad2deg(double(sol_theta5(2)));
+    left_ang_deg = rad2deg(double(sol_thetaL(1)));
+    right_ang_deg = rad2deg(double(sol_thetaR(2)));
 end
