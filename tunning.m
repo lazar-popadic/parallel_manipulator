6cb25b5cb670b9f0c7b733137223f4a434758091
lazuskica <lazuskica@gmail.com>
2024-08-28 10:37:13 +0200

    mali update

--- tunning.m
+++ tunning.m
@@ -1,9 +1,9 @@
 function cost = tunning(K)
 assignin('base', 'K', K);
-P = K(1)/8
-I = K(2)/50
-D = K(3)/200
+P = K(1)
+I = K(2)
+D = K(3)
 sim("five_bar.slx");
-cost = ITAE1(length(ITAE1))
+cost = IAE1(length(IAE1))
 disp("____________");
 end
